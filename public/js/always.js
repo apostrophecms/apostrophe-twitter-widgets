1d510687b0c4bcf7986a4f8814bfb4eb04d2420f
Austin Starin <austin.starin@gmail.com>
2017-10-25 13:40:46 -0400

    Use self.html() for post and scope to widget

--- public/js/always.js
+++ public/js/always.js
@@ -1,14 +1,11 @@
 apos.define('apostrophe-twitter-widgets', {
   extend: 'apostrophe-widgets',
-  construct: function(self, options) {
-    self.play = function($widget, data, options) {
-
-      $.post('/modules/apostrophe-twitter-widgets/feed', data)
-        .done(function(data) {
-          $('[data-apos-twitter-contents]').html(data).addClass('loaded');
-          $widget.trigger('aposTwitterReady');
-        });
-
+  construct: function (self, options) {
+    self.play = function ($widget, data, options) {
+      self.html('feed', data, function (data) {
+        $widget.find('[data-apos-twitter-contents]').html(data).addClass('loaded');
+        $widget.trigger('aposTwitterReady');
+      });
     };
   }
 });
